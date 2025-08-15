ee6c0e7695d7a16bd25768c0cc2b4f318d67410f
alexnorniella <alexnorniella@yahoo.co.uk>
2025-08-15 09:34:55 +0100

    try a different way to change permissions

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /home/ubuntu/acebook-app
 # give read access to package...
-COPY --chown=user:user package-lock.json .
+chmod ugo+r ./package-lock.json
 # Install Node.js dependencies
 npm install
